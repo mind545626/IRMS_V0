38cb675750493373b2abfef45f1ceea54d03dbd1
LAPTOP-7N99C0GA\ASUS <mind545626>
2020-10-01 04:35:56 +0800

    圖檔縮小預置

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,31 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // template:`
+  // <div class="container">
+  //   <div class="masthead-subheading">Welcome To Our Studio!</div>
+  //   <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
+  //   <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
+  // </div>
+
+
+  // <footer class="footer py-4">
+  //   <div class="container">
+  //     <div class="row align-items-center">
+  //       <div class="col-lg-4 text-lg-left">Copyright © Your Website 2020</div>
+  //       <div class="col-lg-4 my-3 my-lg-0">
+  //         <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
+  //         <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
+  //         <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
+  //       </div>
+  //       <div class="col-lg-4 text-lg-right">
+  //         <a class="mr-3" href="#!">Privacy Policy</a>
+  //         <a href="#!">Terms of Use</a>
+  //       </div>
+  //     </div>
+  //   </div>
+  // </footer>
+  // `,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
